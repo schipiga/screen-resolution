d943d306f81117673cc770becbddd59a8f4ee92a
Sergei Chipiga <chipiga86@gmail.com>
2017-10-06 15:29:06 +0300

    Add cache option

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
+For Linux you should have `imagemagick` installed.
+
+Simple example:
+
 ```js
 var resolution = require("screen-resolution");
 resolution.get().then(result => console.log(result));
+```
+
+By default it caches result. In order to clear:
+
+```js
+resolution.clear();
+```
+
+Or disable cache:
+
+```js
+resolution.get(false);
 ```
--- index.js
+++ index.js
@@ -20,13 +20,13 @@ var resolution = {};
  *
  * @function
  * @async
+ * @arg {boolean} [cache=true] - Flag to cache result.
  * @return {object} - Dictionary with keys `width` & `height`.
  */
-module.exports.get = () => {
-    if (!_.isEmpty(resolution)) return Promise.resolve(resolution);
-
+module.exports.get = cache => {
+    cache = cache === false ? false : true;
+    if (cache && !_.isEmpty(resolution)) return Promise.resolve(resolution);
     var screenPath = temp.path({suffix: ".png"});
-
     return new Promise((resolve, reject) => {
         screenshot(screenPath, err => {
             if (err) reject(err);
@@ -34,11 +34,17 @@ module.exports.get = () => {
                 .createReadStream(screenPath)
                 .pipe(new PNG())
                 .on("parsed", function() {
-                    resolution.width = this.width;
-                    resolution.height = this.height;
-                    resolve(resolution);
+                    var result = { width: this.width, height: this.height };
+                    if (cache) resolution = result;
+                    resolve(result);
                 })
                 .on("error", reject);
         });
     });
 };
+/**
+ * Clears cached result.
+ *
+ * @function
+ */
+module.exports.clear = () => { resolution = {} };
